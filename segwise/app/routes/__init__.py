731ea03baf3bca4992a4f85f7a18876bc80504f2
Rupesh Kumar <rupeshkumar@Rupeshs-MacBook-Air.local>
2024-12-14 23:03:17 +0530

    UI elements added for Interaction

--- segwise/app/routes/__init__.py
+++ segwise/app/routes/__init__.py
@@ -2,9 +2,11 @@
 # from .setup import setup_db_schema
 # from .upload_csv import upload_csv_route, upload_csv_with_url
 from .upload_csv import upload_csv_with_url
+from .home import home_route
 
 def init_routes(app):
     # Register the setup and upload_csv routes
     # setup_db_schema(app)
     # upload_csv_route(app=app)
+    home_route(app)
     upload_csv_with_url(app=app)
